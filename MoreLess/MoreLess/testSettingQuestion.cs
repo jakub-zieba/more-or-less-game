0be1944f6143ae75e90c119ed55e5389d5fef084
Dawid Sieczka <dawid.sieczka.praca@gmail.com>
2019-02-07 21:56:29 +0100

    Set Questions

--- MoreLess/MoreLess/testSettingQuestion.cs
+++ MoreLess/MoreLess/testSettingQuestion.cs
@@ -41,23 +41,23 @@ namespace MoreLess
             q = new QuestionTemplate[100];
             value = 0;
         }
-        public string Question(int i)
+        public QuestionTemplate Question(int i)
         {
             q[0] = new QuestionTemplate ("Spanish-flu killed 150 million people.", less , 4);
-            q[1] = new QuestionTemplate("Freddie Mercury died in 1992.",;
-            q[2] = "the highest point on the world has - 8848m ";
-            q[3] = "the lowest point on the worls has 9873m ";
-            q[4] = "the heviest  animal on the world wieighs - 100 tons ";
-            q[5] = "the smallest country in the world has 1 square kilometer.   ";
-            q[6] = "water surface on the planet is 65%  ";
-            q[7] = "the highest man in the world was ";
-            q[8] = "amazon has 7100km lenght";
-            q[9] = "the number of facebook users is 1,500,000,000 ";
-            q[10] = "Poland was under 110 years of partition  ";
-            q[11] = "the population in Mongolia is 3 millions ";
-            q[12] = "elephants live 50 years ";
-            q[13] = "43 million people live in Poland ";
-            q[14] = "1,5 billions people have no access to water ";
+            q[1] = new QuestionTemplate("Freddie Mercury died in 1992.",less,1);
+            q[2] = new QuestionTemplate("the highest point on the world has - 8848m ", equals ,3);
+            q[3] = new QuestionTemplate("the lowest point on the worls has 9873m ",more ,2);
+            q[4] = new QuestionTemplate("the heviest  animal on the world wieighs - 100 tons", more, 4);
+            q[5] = new QuestionTemplate("the smallest country in the world has 1 square kilometer.", less ,3);
+            q[6] = new QuestionTemplate("water surface on the planet is 65%", more ,4);
+            q[7] = new QuestionTemplate("the highest man in the world was 272cm ", equals ,3);
+            q[8] = new QuestionTemplate("amazon has 7100km lenght", less ,4);
+            q[9] = new QuestionTemplate("the number of facebook users is 1,500,000,000", more ,4);
+            q[10] =new QuestionTemplate( "Poland was under 110 years of partition",more ,5);
+            q[11] =new QuestionTemplate( "the population in Mongolia is 3 millions", equals ,2);
+            q[12] =new QuestionTemplate( "elephants live 50 years ", more ,5);
+            q[13] =new QuestionTemplate( "43 million people live in Poland", less ,4);
+            q[14] = new QuestionTemplate("1,5 billions people have no access to water", more ,3);
 
             return q[i];
         }
