145ff118beafbbb4f69de55e67cdba5c78466f73
kalszym <kalszym@op.pl>
2019-02-10 02:45:06 +0100

    testy

---
+++ unit test/QuestionDisplayerTests.cs
@@ -0,0 +1,56 @@
+using ClassLibrary;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace UnitTestProject3
+{
+    [TestClass]
+    public class QuestionDisplayerTests
+    {
+
+
+        [TestInitialize]
+        public void TestInitialize()
+        {
+
+        }
+
+
+        private QuestionDisplayer CreateQuestionDisplayer()
+        {
+            return new QuestionDisplayer();
+        }
+
+        [TestMethod]
+        public void GetQuestion_StateUnderTest_ExpectedBehavior()
+        {
+           
+            var unitUnderTest = this.CreateQuestionDisplayer();
+            int i = 1;
+
+         
+            var result = unitUnderTest.GetQuestion(
+                i);
+
+            
+            Assert.Fail();
+        }
+
+        [TestMethod]
+        public void CheckAnswer_StateUnderTest_ExpectedBehavior()
+        {
+          
+            var unitUnderTest = this.CreateQuestionDisplayer();
+            Answer _currentAnswer = 0;
+            Answer _correctAnswer = 0;
+
+           
+            var result = unitUnderTest.CheckAnswer(
+                _currentAnswer,
+                _correctAnswer);
+
+        
+            Assert.Fail();
+        }
+    }
+}
